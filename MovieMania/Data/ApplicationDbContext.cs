be5f57a57e959df69eb39d49299c83dddee9672a
dac <dac@dypm.org>
2023-07-26 16:13:16 +0530

    Basic CRUD + Sorting By ASP.NET Core MVC By Entity Framework

---
+++ MovieMania/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MovieMania.Models;
+
+namespace MovieMania.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options){ 
+        
+        }
+
+        public DbSet<Movie> Movies { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
